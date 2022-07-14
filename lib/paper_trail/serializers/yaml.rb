5c211146f40380834dc192deab645d596638ccef
Tim Connor <tim.connor@appfolio.com>
2022-07-15 10:59:40 +1200

    WIP: example yaml load fix

--- lib/paper_trail/serializers/yaml.rb
+++ lib/paper_trail/serializers/yaml.rb
@@ -9,7 +9,13 @@ module PaperTrail
       extend self # makes all instance methods become module methods as well
 
       def load(string)
-        ::YAML.respond_to?(:unsafe_load) ? ::YAML.unsafe_load(string) : ::YAML.load(string)
+        if active_record_use_safe_load?
+          YAML.safe_load(string, permitted_classes: yaml_permitted_classes, aliases: true)
+        elsif ::YAML.respond_to?(:unsafe_load)
+          ::YAML.unsafe_load(string)
+        else
+          ::YAML.load(string)
+        end
       end
 
       # @param object (Hash | HashWithIndifferentAccess) - Coming from
@@ -25,6 +31,28 @@ module PaperTrail
       # value in the serialized object.
       def where_object_condition(arel_field, field, value)
         arel_field.matches("%\n#{field}: #{value}\n%")
+      end
+
+      private
+
+      def active_record_use_safe_load?
+        if ActiveRecord.respond_to?(:use_yaml_unsafe_load)
+          !ActiveRecord.use_yaml_unsafe_load
+        elsif ActiveRecord::Base.respond_to?(:use_yaml_unsafe_load)
+          !ActiveRecord::Base.use_yaml_unsafe_load
+        else
+          false
+        end
+      end
+
+      def yaml_permitted_classes
+        if ActiveRecord.respond_to?(:yaml_column_permitted_classes)
+          ActiveRecord.yaml_column_permitted_classes
+        elsif ActiveRecord::Base.respond_to?(:yaml_column_permitted_classes)
+          ActiveRecord::Base.yaml_column_permitted_classes
+        else
+          []
+        end
       end
     end
   end
